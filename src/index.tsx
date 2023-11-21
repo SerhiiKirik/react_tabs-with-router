8f0b9c26c3d51ef8cca9570a6a7cc71d0e64c4dd
Sergey Kirichenko <s.kirichenko@m-dev.org>
2023-11-21 13:34:54 +0200

    solution with updated deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { App } from './App';
+
+import { Root } from './Root';
 
 const container = document.getElementById('root') as HTMLElement;
 
-createRoot(container).render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-);
+createRoot(container)
+  .render(<Root />);
